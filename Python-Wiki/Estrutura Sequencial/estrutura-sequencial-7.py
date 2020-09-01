9081c7a0b76de963d26371b28b67bd0d8a24c2f9
pedrokaroth <56929916+pedrokaroth@users.noreply.github.com>
2020-09-01 09:29:55 -0300

    EstruturaSequencial
    Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.

---
+++ Python-Wiki/Estrutura Sequencial/estrutura-sequencial-7.py
@@ -0,0 +1,3 @@
+base = float(input("Digite a base do quadrado: "))
+altura = float(input("Digite a alutra do quadrado: "))
+print("A area do quadrado é: " + str(base*altura) + " e o dobro: " + str((base*altura) * 2))
