feabf8041819cf168834dcd56289d9f1e3fc4b24
pedrokaroth <56929916+pedrokaroth@users.noreply.github.com>
2020-09-02 08:11:12 -0300

    EstruturaSequencial
    Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
    o produto do dobro do primeiro com metade do segundo .
    a soma do triplo do primeiro com o terceiro.
    o terceiro elevado ao cubo.

---
+++ Python-Wiki/Estrutura Sequencial/estrutura-sequencial-11.py
@@ -0,0 +1,8 @@
+num1 = int(input("(int) 1º Número: "))
+num2 = int(input("(int) 2º Número: "))
+num3 = float(input("(float) 3 Número: "))
+
+print("a) Produto do dobro do primeiro com metade do segundo: " + str((num1 * 2) * (num2 / 2)))
+print("b) Soma do triplo do primeiro com o terceiro: " + str( (num1 * 3)  + num3))
+print("c) O terceiro elevado ao cubo: " + str( num3 * num3 * num3 ))
+
