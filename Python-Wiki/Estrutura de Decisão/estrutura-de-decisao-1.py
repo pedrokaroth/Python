5fe41b856b320d9367d3feeb717212df3e335986
pedrokaroth <56929916+pedrokaroth@users.noreply.github.com>
2020-09-08 11:30:20 -0300

    EstruturaDeDecisao
    Faça um Programa que peça dois números e imprima o maior deles.

---
+++ Python-Wiki/Estrutura de Decisão/estrutura-de-decisao-1.py
@@ -0,0 +1,8 @@
+valorA = int(input("Digite um valor: "))
+valorB = int(input("Digite um valor: "))
+
+if valorA > valorB:
+	print(str(valorA) + " é maior que " + str(valorB))
+else:
+	print(str(valorB) + " é maior que " + str(valorA))
+	
