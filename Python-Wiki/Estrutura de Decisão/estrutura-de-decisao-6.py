32387c0a3b21450cf9cade2ee2374df1a9405502
Pedro Dimbarre <pedrokaroth@gmail.com>
2020-09-16 08:53:05 -0300

    EstruturaDeDecisao
    Faça um Programa que leia três números e mostre o maior deles.

---
+++ Python-Wiki/Estrutura de Decisão/estrutura-de-decisao-6.py
@@ -0,0 +1,13 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+c = int(input("Digite um número: "))
+
+if(a > b > c):
+	print("Maior valor: ", a)
+
+elif b > c:
+	print("Maior valor: ", b)
+
+else:
+	print("Maior valor: ", c)
+
