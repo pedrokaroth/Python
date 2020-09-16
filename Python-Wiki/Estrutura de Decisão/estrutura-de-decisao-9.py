05ef55d7a6986666e32192ea429db449a12da814
Pedro Dimbarre <pedrokaroth@gmail.com>
2020-09-16 09:16:56 -0300

    EstruturaDeDecisao
    Faça um Programa que leia três números e mostre-os em ordem decrescente.

---
+++ Python-Wiki/Estrutura de Decisão/estrutura-de-decisao-9.py
@@ -0,0 +1,9 @@
+numeros = []
+
+numeros.append(int(input("Digite um número: ")))
+numeros.append(int(input("Digite um número: ")))
+numeros.append(int(input("Digite um número: ")))
+
+# Função nativa para organizar os números em ordem sorted()
+
+print(sorted(numeros))
