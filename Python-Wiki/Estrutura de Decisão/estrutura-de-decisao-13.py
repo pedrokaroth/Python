c756cd27657a0a01bdb259c97d46a22709b77ec8
Pedro Dimbarre <pedrokaroth@gmail.com>
2020-09-22 15:32:35 -0300

    EstruturaDeDecisao

---
+++ Python-Wiki/Estrutura de Decisão/estrutura-de-decisao-13.py
@@ -0,0 +1,25 @@
+dia = int(input("Dia da semana: "))
+
+if dia == 1:
+	print("Domingo")
+
+elif dia == 2:
+	print("Segunda-feira")
+
+elif dia == 3:
+	print("Terça-feira")
+
+elif dia == 4:
+	print("Quarta-feira")
+
+elif dia == 5:
+	print("Quinta-feira")
+
+elif dia == 6:
+	print("Sexta-feira")
+
+elif dia == 7:
+	print("Sabado")
+
+else:
+	print("Valor inválido")
